e8901d741f6765ce4371ee9fd49da54ae8e9dceb
muxinglin <linmuxing@sjtu.edu.cn>
2023-01-21 22:39:41 -0800

    Code for hello world

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print("view has been loaded")
         // Do any additional setup after loading the view.
     }
 
