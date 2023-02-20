2997de22641ea9a254a9604c85e9c458eaf2076f
muxinglin <linmuxing@sjtu.edu.cn>
2023-02-19 22:41:39 -0800

    a02

--- Assignment02/Assignment02/ViewController.swift
+++ Assignment02/Assignment02/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let restaurant = ["Zaika", "Hotpot", "Zeeks Pizza", "NEU"]
+    
+    let food = [["Food1-Zaika", "Food2-Zaika", "Food3-Zaika", "Food4-Zaika"], ["Food1-Hotpot", "Food2-Hotpot", "Food3-Hotpot"],["Food1-Zeeks Pizza", "Food2-Zeeks Pizza", "Food3-Zeeks Pizza", "Food4-Zeeks Pizza", "Food5-Zeeks Pizza"],["Food1-NEU", "Food2-NEU"]]
+    
+    @IBOutlet weak var tblViewTop: UITableView!
+    
+    @IBOutlet weak var tblViewBottom: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if tableView == tblViewTop {
+            return restaurant.count
+        } else {
+            if let selectedRestaurantIndex = tblViewTop.indexPathForSelectedRow?.row {
+                return food[selectedRestaurantIndex].count
+            }
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == tblViewTop {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = restaurant[indexPath.row]
+            return cell
+            
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "footItemCell", for: indexPath)
+            if let selectedRestaurantIndex = tblViewTop.indexPathForSelectedRow?.row {
+                cell.textLabel?.text = food[selectedRestaurantIndex][indexPath.row]
+            }
+            return cell
+            
+        }
+        
+        
 
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView == tblViewTop {
+            let selectedRestaurant = restaurant[indexPath.row]
+            let selectedFoodItems = food[indexPath.row]
+            tblViewBottom.reloadData()
+        }
+    }
+    
 
+    
 }
 
